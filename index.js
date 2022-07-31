5c2d5d26cacd2aab82c0b8864aa8c710049c4da7
Kartikey <69628973+Kartiky246@users.noreply.github.com>
2022-07-31 14:37:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ cards.forEach((card) => {
 
 function end() {
 
-if(count>40){
+if(count>0){
   let displayWinner = document.querySelector(".container");
   displayWinner.innerHTML = "";
   let txt = document.createTextNode(`Number of Moves: ${count}`);
